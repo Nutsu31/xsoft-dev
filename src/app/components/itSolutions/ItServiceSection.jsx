f23685bdf18d3cc863c4a6aaa97ff8960752c338
Nutsu31 <aleksandrenutsubidze@gmail.com>
2023-07-27 16:09:50 +0400

    ref: refactore exist object to an array

--- src/app/components/itSolutions/ItServiceSection.jsx
+++ src/app/components/itSolutions/ItServiceSection.jsx
@@ -1,69 +1,79 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import ItService from "./ItService";
 import Styles from "./ItServiceSection.module.css";
-const ittext1 = {
-  header: "Web development",
-  info: "Development of sites and web applications of any complexity",
-  info2: "Consulting",
-  info3: "UI/UX design",
-  dev: "Turkey development",
-  boxNumber: 1,
-};
 
-const ittext2 = {
-  header: "Mobile development",
-  info: "Android / iOS mobile application development",
-  info2: "Consulting",
-  info3: "UI/UX design",
-  dev: "Turkey development",
-  boxNumber: 2,
-};
-
-const ittext3 = {
-  header: "Boxed solutions",
-  info: "Development of sites and web applications of any complexity",
-  info2: "Consulting",
-  info3: "UI/UX design",
-  dev: "Development from 2 weeks",
-  boxNumber: 3,
-};
-
-const ittext4 = {
-  header: "Outstaff",
-  info: "We allocate developers for your tasks.",
-  info2: "Adaptation",
-  info3: "Working on your project",
-  dev: "Flexible prices",
-  boxNumber: 4,
-};
-
-const ittext5 = {
-  header: "No code solution",
-  info: "Technical support and service 24/7.",
-  info2: "Consulting",
-  info3: "UI/UX design",
-  dev: "Turkey development",
-  boxNumber: 5,
-};
-
-const ittext6 = {
-  header: "Support",
-  info: "Technical support and service 24/7.",
-  info2: "Reduced product downtime",
-  info3: "Assistance in restoring the functionality of the product",
-  dev: "Prompt response to problems",
-  boxNumber: 6,
-};
+const textArr = [
+  {
+    text: {
+      header: "Web development",
+      info: "Development of sites and web applications of any complexity",
+      info2: "Consulting",
+      info3: "UI/UX design",
+      dev: "Turkey development",
+      boxNumber: 1,
+    },
+  },
+  {
+    text: {
+      header: "Mobile development",
+      info: "Android / iOS mobile application development",
+      info2: "Consulting",
+      info3: "UI/UX design",
+      dev: "Turkey development",
+      boxNumber: 2,
+    },
+  },
+  {
+    text: {
+      header: "Boxed solutions",
+      info: "Development of sites and web applications of any complexity",
+      info2: "Consulting",
+      info3: "UI/UX design",
+      dev: "Development from 2 weeks",
+      boxNumber: 3,
+    },
+  },
+  {
+    text: {
+      header: "Outstaff",
+      info: "We allocate developers for your tasks.",
+      info2: "Adaptation",
+      info3: "Working on your project",
+      dev: "Flexible prices",
+      boxNumber: 4,
+    },
+  },
+  {
+    text: {
+      header: "No code solution",
+      info: "Technical support and service 24/7.",
+      info2: "Consulting",
+      info3: "UI/UX design",
+      dev: "Turkey development",
+      boxNumber: 5,
+    },
+  },
+  {
+    text: {
+      header: "Support",
+      info: "Technical support and service 24/7.",
+      info2: "Reduced product downtime",
+      info3: "Assistance in restoring the functionality of the product",
+      dev: "Prompt response to problems",
+      boxNumber: 6,
+    },
+  },
+];
 
 const ItServiceSection = () => {
   return (
     <div className={Styles.grid}>
-      <ItService text={ittext1} />
-      <ItService text={ittext2} />
-      <ItService text={ittext3} />
-      <ItService text={ittext4} />
-      <ItService text={ittext5} />
-      <ItService text={ittext6} />
+      {textArr.map((item) => {
+        return (
+          <ItService key={Math.random() * Math.random()} text={item.text} />
+        );
+      })}
     </div>
   );
 };
--- src/app/components/probleSolvingSection/ProblemSolvingSection.jsx
+++ src/app/components/probleSolvingSection/ProblemSolvingSection.jsx
@@ -1,46 +1,60 @@
 import React from "react";
 import ServiceBox from "./ServiceBox";
 import Styles from "./ProblemSolvingSection.module.css";
-const text1 = {
-  header: "Business process optimization",
-  info: "Increase productivity and business efficiency",
-  boxNumber: 1,
-};
-const text2 = {
-  header: "Increasing sales",
-  info: "A convenient shopping environment is the key to high sales.",
-  boxNumber: 2,
-};
-const text3 = {
-  header: "Detachment from competitors",
-  info: "Be the first in the industry! Stand out from competitors with the help of IT-technologies.",
-  boxNumber: 3,
-};
-const text4 = {
-  header: "Start up",
-  info: "The solution for a quick start of your new product.",
-  boxNumber: 4,
-};
-const text5 = {
-  header: "Loyalty increase",
-  info: "Increasing the company's turnover through long-term customer relationships.",
-  boxNumber: 5,
-};
-const text6 = {
-  header: "Reputation",
-  info: "Boost your reputation and brand awareness.",
-  boxNumber: 6,
-};
+
+const textArr = [
+  {
+    text: {
+      header: "Business process optimization",
+      info: "Increase productivity and business efficiency",
+      boxNumber: 1,
+    },
+  },
+  {
+    text: {
+      header: "Increasing sales",
+      info: "A convenient shopping environment is the key to high sales.",
+      boxNumber: 2,
+    },
+  },
+  {
+    text: {
+      header: "Detachment from competitors",
+      info: "Be the first in the industry! Stand out from competitors with the help of IT-technologies.",
+      boxNumber: 3,
+    },
+  },
+  {
+    text: {
+      header: "Start up",
+      info: "The solution for a quick start of your new product.",
+      boxNumber: 4,
+    },
+  },
+  {
+    text: {
+      header: "Loyalty increase",
+      info: "Increasing the company's turnover through long-term customer relationships.",
+      boxNumber: 5,
+    },
+  },
+  {
+    text: {
+      header: "Reputation",
+      info: "Boost your reputation and brand awareness.",
+      boxNumber: 6,
+    },
+  },
+];
 
 const ProblemSolvingSection = () => {
   return (
     <div className={Styles.grid}>
-      <ServiceBox text={text1} />
-      <ServiceBox text={text2} />
-      <ServiceBox text={text3} />
-      <ServiceBox text={text4} />
-      <ServiceBox text={text5} />
-      <ServiceBox text={text6} />
+      {textArr.map((item) => {
+        return (
+          <ServiceBox key={Math.random() * Math.random()} text={item.text} />
+        );
+      })}
     </div>
   );
 };
