a20da5782846fb3eb7347366ea7cb91bb60c149e
Nutsu31 <aleksandrenutsubidze@gmail.com>
2023-07-27 22:07:44 +0400

    feat: added new photos and sections

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -2,28 +2,29 @@ import React from "react";
 import "./Header.scss";
 import Image from "next/image";
 import Logo from "../../assets/Logo.jpg";
-import Link from 'next/link';
-
+import Logo1 from "@/app/assets/xsoft12.png";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header>
       <div className="header-container">
         <div className="logo">
-        <Link href="/">
-          <Image
-            src={Logo}
-            alt="Picture of the author"
-          />
-          
+          <Link href="/">
+            <Image src={Logo1} alt="Picture of the author" />
           </Link>
         </div>
         <div className="menu">
           <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
-            
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
           </ul>
         </div>
       </div>
