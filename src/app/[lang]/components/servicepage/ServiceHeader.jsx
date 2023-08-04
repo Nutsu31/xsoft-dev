52b30606138ae46bad486475d350a4800292e7c4
jaba777 <tskhovreba777@gmail.com>
2023-08-04 08:07:28 -0800

    add scroll on serverpage

--- src/app/[lang]/components/servicepage/ServiceHeader.jsx
+++ src/app/[lang]/components/servicepage/ServiceHeader.jsx
@@ -1,16 +1,25 @@
+"use client";
 import React from "react";
 import Styles from "./styles/ServiceHeader.module.css";
 import Image from "next/image";
 import bg from "@/app/[lang]/assets/w1.png";
-const ServiceHeader = ({lang}) => {
+import { Link as LinkScroll } from "react-scroll";
+const ServiceHeader = ({ lang }) => {
   return (
     <div className={Styles.header}>
       <h1 className={Styles.h1}>{lang.title}</h1>
-      <p className={Styles.p}>
-        {lang.text}
-      </p>
-     
-      <button className={Styles.btn}>{lang.consultation}</button>
+      <p className={Styles.p}>{lang.text}</p>
+      <LinkScroll
+        activeClass="active"
+        to="contactUs"
+        spy={true}
+        smooth={true}
+        offset={-150}
+        duration={500}
+      >
+        {" "}
+        <button className={Styles.btn}>{lang.consultation}</button>
+      </LinkScroll>
       <Image src={bg} alt="bg" className={Styles.bg} />
     </div>
   );
